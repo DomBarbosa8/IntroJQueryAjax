158ad68aada86b4147b0ba72029b49f71c228cf9
DomBarbosa8 <mobarbosa.phoenix@gmail.com>
2021-09-21 21:39:20 -0300

    intro jquery com buscador de cep

---
+++ js/main.js
@@ -0,0 +1,27 @@
+function consultaCep(){
+    $(".barra-progresso").show();
+    let cep = document.getElementById("cep").value;
+    let url = "https://viacep.com.br/ws/" + cep + "/json/";
+    console.log(url)
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function(response){
+            console.log(response);
+            $("#logradouro").html(response.logradouro);
+            $("#bairro").html(response.bairro);
+            $("#localidade").html(response.localidade);
+            $("#uf").html(response.uf);
+            $("#tituloCep").html("CEP " + response.cep);
+            $(".cep").show();
+            $(".barra-progresso").hide();
+            //$("#logradouro").html(response.logradouro);
+            //document.getElementById("logradouro").innerHTML = response.logradouro;
+            //alert(response.logradouro);
+        }
+    })
+}
+$(function(){
+    $(".cep").hide();
+    $(".barra-progresso").hide();
+});
